6ce878d4d1b0fbb2355277b20c4ba782a04c6361
Даниил Аленин <aleeenin.daniil@yandex.ru>
2026-03-26 23:30:08 +0300

    Initial commit

---
+++ src/main/kotlin/itmo/medsoft/MedsoftApplication.kt
@@ -0,0 +1,11 @@
+package itmo.medsoft
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedsoftApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedsoftApplication>(*args)
+}
---
+++ src/test/kotlin/itmo/medsoft/MedsoftApplicationTests.kt
@@ -0,0 +1,13 @@
+package itmo.medsoft
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MedsoftApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
